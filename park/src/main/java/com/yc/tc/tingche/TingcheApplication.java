5824c7d7b90a065355700ae75323cf402321ed61
OrzFeel00 <632360579@qq.com>
2020-09-22 15:18:23 +0800

    框架初次搭建

---
+++ park/src/main/java/com/yc/tc/tingche/TingcheApplication.java
@@ -0,0 +1,13 @@
+package com.yc.tc.tingche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TingcheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TingcheApplication.class, args);
+	}
+
+}
