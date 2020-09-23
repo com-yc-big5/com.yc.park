b23b03912dadf977ff3d05554542de0ecea88e0d
OrzFeel00 <632360579@qq.com>
2020-09-23 17:10:01 +0800

    框架搭建---修改Javabean

--- park/src/main/java/com/yc/tc/tingche/bean/park.java
+++ park/src/main/java/com/yc/tc/tingche/bean/park.java
@@ -3,7 +3,7 @@ package com.yc.tc.tingche.bean;
 public class park {
 
 	private Integer id;
-	private Integer garage_id ;
+	private String garage ;
 	private double price;
 	private Integer status ;
 	private String size ;
@@ -14,53 +14,64 @@ public class park {
 		// TODO Auto-generated constructor stub
 	}
 
-	
-	public park(Integer id, Integer garage_id, double price, Integer status, String size, Integer isLong) {
-		super();
-		this.id = id;
-		this.garage_id = garage_id;
-		this.price = price;
-		this.status = status;
-		this.size = size;
-		this.isLong = isLong;
+	public Integer getId() {
+		return id;
 	}
 
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getGarage() {
+		return garage;
+	}
+
+	public void setGarage(String garage) {
+		this.garage = garage;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
 
 	public String getSize() {
 		return size;
 	}
+
 	public void setSize(String size) {
 		this.size = size;
 	}
+
 	public Integer getIsLong() {
 		return isLong;
 	}
+
 	public void setIsLong(Integer isLong) {
 		this.isLong = isLong;
 	}
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
+
+	public park(Integer id, String garage, double price, Integer status, String size, Integer isLong) {
+		super();
 		this.id = id;
-	}
-	public Integer getGarage_id() {
-		return garage_id;
-	}
-	public void setGarage_id(Integer garage_id) {
-		this.garage_id = garage_id;
-	}
-	public double getPrice() {
-		return price;
-	}
-	public void setPrice(double price) {
+		this.garage = garage;
 		this.price = price;
-	}
-	public Integer getStatus() {
-		return status;
-	}
-	public void setStatus(Integer status) {
 		this.status = status;
+		this.size = size;
+		this.isLong = isLong;
 	}
+
+	
 	
 }
