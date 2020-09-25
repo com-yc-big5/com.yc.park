9898cc7aa22b09125ecb51bdfd8210eeca552957
joyerush <1838750597@qq.com>
2020-09-25 23:21:22 +0800

    后台页面上传（并且已实现页面跳转）

--- park/src/main/java/com/yc/tc/tingche/web/IndexAction.java
+++ park/src/main/java/com/yc/tc/tingche/web/IndexAction.java
@@ -1,8 +1,10 @@
 package com.yc.tc.tingche.web;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class IndexAction {
 	
 	@GetMapping("/")
@@ -10,4 +12,47 @@ public class IndexAction {
 		return "index";
 	}
 
+/****************管理员页面     开始****************/
+	//后台首页
+	@GetMapping(path= {"admin","index.html"})
+	public String AdIndex() {
+		return "admin/index";
+	}
+	//后台车辆入库页面
+	@GetMapping("inpart.html")
+	public String AdInpart() {
+		return "admin/inpart";
+	}
+	//后台车辆出库页面
+	@GetMapping("outpart.html")
+	public String AdOutpart() {
+		return "admin/outpart";
+	}
+	//后台车位管理页面
+	@GetMapping("part.html")
+	public String AdPart() {
+		return "admin/part";
+	}
+	//后台用户管理页面
+	@GetMapping("user.html")
+	public String AdUser() {
+		return "admin/user";
+	}
+	//后台订单管理页面
+	@GetMapping("order.html")
+	public String AdOrder() {
+		return "admin/order";
+	}
+	//后台管理员登录页面
+	@GetMapping("login.html")
+	public String AdLogin() {
+		return "admin/login";
+	}
+	//后台个人中心页面
+	@GetMapping("person.html")
+	public String AdPerson() {
+		return "admin/person";
+	}
+/****************管理员页面     结束****************/
+	
 }
